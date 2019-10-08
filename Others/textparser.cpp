e810d7c2cfc0c2b90525ee9d847b5fadb66b758c
Christopher Brown <cb719@cb719-laptop.hep.ph.ic.ac.uk>
2019-10-08 11:58:54 +0100

    Textparser with sort

--- Others/textparser.cpp
+++ Others/textparser.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <iterator>
 #include <exception>
 #include <vector>
+#include <list>
 #include <algorithm>
 
 using namespace std;
+
+
+template<typename I> 
+void my_sort(I first, I last)
+{
+    I original = first;
+    while (first!=(last-1)){
+        if (*first >= *(first+1))
+            ++first;
+        else {
+            auto temp = *(first+1);
+            *(first+1)  = *first;
+            *first = temp;
+            first = original; 
+        }
+    }
+    return;
+}
+
+
+
 
 int main(int argc, char *argv[]){
     string iname = argv[1];
@@ -17,7 +40,14 @@ int main(int argc, char *argv[]){
         reading.push_back(buff);
     } 
 
-    sort(reading.begin(),reading.end());
+    list<int> my_list = {04,1,21,3,43,5,7,56,8,19};
+
+    my_sort(reading.begin(),reading.end());
+
+    my_list.sort();
+
+    copy(my_list.begin(),my_list.end(),ostream_iterator<int>(cout, " "));
+
 
     for (int i=0; i<reading.size(); ++i){
         if (i%5 == 0) cout << '\n'; 
