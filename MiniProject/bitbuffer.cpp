4d08a3a931c662a247ad3a929199023c684ad806
Christopher Brown <cb719@cb719-laptop.hep.ph.ic.ac.uk>
2019-10-11 16:26:52 +0100

    now wit working hit class

--- MiniProject/bitbuffer.cpp
+++ MiniProject/bitbuffer.cpp
@@ -5,135 +5,123 @@
 #include <vector>
 #include <chrono>
 
-
-class bits
-{
-    private:
-        char* mem_buff;
-    public:
-
-        int n_bits;
-
-        bits() {};
-
-        bits(int n) : n_bits{n} , mem_buff{new char[n]}
-        {
-            for (int i=0; i<n; ++ i) mem_buff[i]=0;
-        }
-
-        ~bits()
-        {
-            delete mem_buff;
-        }
-
-        int decode();
-
-        void print();
-
-        bits reading(bits&);
-
-};
-
-int bits::decode(){
-    return int(*mem_buff); 
-     
-};
-
-void bits::print(){
-    std::cout<<*mem_buff << '\n';
-};
-
-/*
-
 class hit
 {
     public:
-        hit() {
-            x_coordinate = bits(3);
-            y_coordinate = bits(3);
-            drift_time = bits(10);
-        };
+        hit() : id{0}, x_coordinate{0} , y_coordinate{0} , drift_time{0} {}; //default constructor
+
+        hit(int number, int x, int y, int t) : id{number}, x_coordinate{x} , y_coordinate{y} , drift_time{t} {}; //constructor
+
+        hit& operator=(const hit &); //copy assignment
+
+        hit(const hit& h) : id{h.id}, x_coordinate{h.x_coordinate} , y_coordinate{h.y_coordinate} , drift_time{h.drift_time} {}; //copy constructor
+
+        void set_values(int n, int x, int y, int t)
+        {
+            id = n;
+            x_coordinate = x;
+            y_coordinate = y;
+            drift_time = t;
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const hit& h);
+        
+
         ~hit(){};
 
-        void fill_hit();
-
     private:
-        bits x_coordinate;
-        bits y_coordinate;
-        bits drift_time;
+        int id;
+        int x_coordinate;
+        int y_coordinate;
+        int drift_time;
 
 };
 
-void hit::fill_hit()
-{
-    x_coordinate.reading();
-    y_coordinate.reading();
-    drift_time.reading();
 
+hit& hit::operator=(const hit& h)
+{
+    id = h.id;
+    x_coordinate = h.x_coordinate;
+    y_coordinate = h.y_coordinate;
+    drift_time = h.drift_time;
+    return *this;
 }
 
 
+std::ostream& operator<<(std::ostream& os, const hit& h)
+        {
+            os << "ID: " << h.id << " X:" << h.x_coordinate << " Y: " << h.y_coordinate << " Drift Time: " << h.drift_time << '\n';
+            return os;
+        }
 
 
-*/
-char reading(int start)
+
+std::vector<int> reading(int start)
 {
-
-    std::ifstream is ("manytracks.raw",std::ios::binary);
-    char r = {' '};
+    
+    std::ifstream is ("onetrack.raw",std::ios::binary);
+    char r[2] = {' ',' '};
+    std::vector<int> return_values = {0,0,0,0};
 
     if (is){
-        is.seekg (start);
-        char* buffer = new char;
-        is.read(buffer,1);
+            for (int j=0;j<2; ++j){
 
-        r = *buffer;
+                is.seekg (start*2+j);
+                char* buffer = new char;
+                is.read(buffer,1);
 
-        delete buffer;
-        }
-    
+                r[j] = *buffer;
+
+                delete buffer;
+            }
+        
+        std::bitset<16> x(r[0]);
+        std::bitset<16> y(r[1]);
+            
+        x = x << 8;
+        x = x ^ y;
+
+        std::bitset<3> x_pos;
+        std::bitset<3> y_pos;
+        std::bitset<10> drift_time;
+
+        for (int k = 0; k < 3; ++k){
+                    x_pos[2-k] = x[15-k];
+                    y_pos[2-k] = x[12-k];
+            }
+        for (int k = 0; k<10; ++k){
+                    drift_time[9-k] = x[9-k];
+            }
+
+        return_values[0] = start;
+        return_values[1] = x_pos.to_ulong();
+        return_values[2] = y_pos.to_ulong();
+        return_values[3] = drift_time.to_ulong();
+
+        
+    }
     is.close();
-    return r;
-
+    return return_values;
 }
 
 
 int main(){
-    for (int k=0; k<1000000; ++k){
-        //std::cout << k << '\n';
+    auto start = std::chrono::high_resolution_clock::now();  
+    for (int i=0; i<8; ++i){
 
-        for (int i=0; i<8; ++i){
-            char r = reading(k+2*i);
-            char r2 = reading(k+2*i+1);
-            
-            std::bitset<16> x(r);
-            std::bitset<16> y(r2);
-            
-            x = x << 8;
-            x = x ^ y;
+        std::vector<int> reading_vals = reading(i);
+        hit h1(reading_vals[0],reading_vals[1],reading_vals[2],reading_vals[3]);
+        std::cout << h1;
 
-            std::bitset<3> x_pos;
-            std::bitset<3> y_pos;
-            std::bitset<10> drift_time;
-
-            for (int j = 0; j<3; ++j){
-                    x_pos[2-j] = x[15-j];
-                    y_pos[2-j] = x[12-j];
-            }
-            for (int j = 0; j<10; ++j){
-                    drift_time[9-j] = x[9-j];
-            }
-
-
-/*
-            std::cout << "X: " << x_pos.to_ulong() << 
-            " Y: " << y_pos.to_ulong() << " Time: " 
-            << drift_time.to_ullong()  << '\n';
-
-  */      
-        
-        }
     }
+
+        
+        
+        
+    
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = finish - start;
+    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
     return 0;
 }
 
