6e571941ccc02e02632e44cf17b7cdaaa030c29b
Christopher Brown <cb719@cb719-laptop.hep.ph.ic.ac.uk>
2019-11-04 13:51:01 +0000

    withoyt big files

--- Pythonscripts/Algothon/Climate/climate.py
+++
@@ -1,9 +0,0 @@
-import quandl
-import pandas as  pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-gender_stats = pd.read_csv("Gender_Stats_csv/Gender_StatsData.csv", sep=',',skiprows=4)
-
-
-print(gender_stats.head())
